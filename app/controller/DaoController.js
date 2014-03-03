7c4a76ec1fcc009deff8b16e505d0daa18c4e84c
Pedro <pedro@pedro-SATELLITE-L735.(none)>
2014-03-03 16:17:46 -0500

    Send the `remaining_attempts` in every score sending

--- app/controller/DaoController.js
+++ app/controller/DaoController.js
@@ -617,6 +617,7 @@ try {
                 }
                 params= {
                     player_code: user.data.uniqueid,
+                    remaining_attempts:  this.careersListController.career.data.max_attempts,
                     activity_id: activityID,
                     score: parseFloat(score),
                     is_passed: successful,
